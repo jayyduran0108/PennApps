16d1bfd03f34250b6b76c596432c51009a23a812
rsrachelsin <33579517+rsrachelsin@users.noreply.github.com>
2018-09-07 23:04:27 -0400

    Add files via upload

---
+++ yeet.py
@@ -0,0 +1,43 @@
+### the floor is fucking lava 
+
+import random
+from tkinter import *
+'''
+model, view, control 
+
+init 
+
+different environments (class/oop)
+
+mousePressed
+
+mouseMoved
+keyPressed 
+timerFired
+
+personWalking
+
+redrawAll
+
+drawstart 
+
+loadImage(data, filename): 
+
+loadGif(data): 
+   '''
+   
+def init(data): 
+    data.mode = 'start'
+    data.
+
+def mousePressed(event,data):
+    pass
+
+def keyPressed(event,data):
+    pass
+
+def timerFired(data):
+    pass
+
+def redrawAll(canvas, data);
+    pass 
